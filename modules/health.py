fd382129828f2538de6f4f9fa3f72cf8054c5941
OliverLinger <olienew32@gmail.com>
2022-10-09 20:12:19 +0100

    An error fix

--- modules/health.py
+++ modules/health.py
@@ -1,4 +1,4 @@
-def lifespan(temperture: float, temperture_norm: float, voltage: float, current: float, drain: float, thermal_runaway: float) -> int:
+def lifespan(temperture: float, temperture_norm: float, voltage: float, current: float, discharge_c: float, thermal_runaway: float) -> int:
     ''' 
     The degradation of the lifespan of the battery is affected by multiple factors.
     These include:
@@ -7,13 +7,13 @@ def lifespan(temperture: float, temperture_norm: float, voltage: float, current:
         current 
         the rate of charge / discharge 
 
-    I am assuming that the discharge rate is the drain variable in this case.
     The higher the voltage and current in the faster the rate of charge.
     Temperture is self explanatory 
+    The rate of discharge will affect the lifespan.
 
     important:
 
         I have added in a variable that does not exist yet in the class and that is the temperture_norm of the battery 
-        Ihave also stated type hinted that the return type will be an int as that is what the lifespan will be represented by.
+        I have also stated type hinted that the return type will be an int as that is what the lifespan will be represented by.
     '''
     pass
