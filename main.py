b18bc724af29ffd3662f1e14f09049eca0c7a3be
Bradley Harris <bradx135@gmail.com>
2022-11-08 17:35:43 +0000

    import changes

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from classes.phone import Phone
 from ms_modules import charging
 from ms_modules.health import lifespan
-from classes import charger 
+from classes.charger import Charger
+from classes.battery import Battery
+from classes.phone import Phone
+from classes.powerbrick import PowerBrick
 
 """
 last_time_discharged: float = None
 """
-phone = Phone(False, False, False, True, 10)
 
-#battery = Battery(1.0, 1.0, 1.0, 1.0)
-#charger = Charger(battery)
-#brick = PowerBrick(1.0, 1.0)
+
+battery = Battery(1.0, 1.0, 1.0, 1.0)
+charger = Charger(battery)
+brick = PowerBrick(1.0, 1.0)
+
+phone = Phone(False, False, False, True, 10, charger)
 
 # we will need to reset some attribtues in battery and in MS when major events happen e.g. stopping charge, stopping discharge
 # we also need to make sure that things are "finished" out when one of these major events happens -- this definitely does not make sense I'll explain in person.
