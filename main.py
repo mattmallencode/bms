ea15faed8e2908398a48002232202ccd992c6b5e
Matt Mallen <120355103@umail.ucc.ie>
2022-11-15 17:17:19 +0000

    Added TTE & TTF to main control loop

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from classes.charger import Charger
 from classes.phone import Phone
 from classes.powerbrick import PowerBrick
 from ms_modules.charging import decide_charge_mode
-from ms_modules.metrics import state_of_charge
+from ms_modules.metrics import state_of_charge, time_till_empty, time_till_full
 import config
 from time import time
 
@@ -12,7 +12,10 @@ battery = Battery(10, 3.6)
 charger = Charger(battery, power_brick)
 phone = Phone(True, True, True, True, 10, charger)
 
+
 config.chargepercent = ((3.6 - config.VOLTAGE_MIN) * 100)/ (config.VOLTAGE_MAX - config.VOLTAGE_MIN)
+config.tte = time_till_empty(phone, config.chargepercent, config.CAPACITY * config.lifespan)
+config.ttf = time_till_full(charger, config.chargepercent, config.CAPACITY * config.lifespan)
 
 while True: 
         phone._root.update_idletasks()
@@ -21,5 +24,6 @@ while True:
             decide_charge_mode(charger)
             charger.charge_battery()
         time_since_last_charge = time()
-        config.chargepercent = state_of_charge(charger, phone, config.chargepercent, time_since_last_charge, config.CAPACITY * config.lifespan)
-        print(battery.current)
+        state_of_charge(charger, phone, config.chargepercent, time_since_last_charge, config.CAPACITY * config.lifespan)
+        time_till_empty(phone, config.chargepercent, config.CAPACITY * config.lifespan)
+        time_till_full(charger, config.chargepercent, config.CAPACITY * config.lifespan)
