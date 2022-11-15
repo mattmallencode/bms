cfdf0da9530bc198dd746ed1e3238ca3e6bc03eb
Bradley Harris <bradx135@gmail.com>
2022-11-15 15:35:56 +0000

    new values in config

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-# The optimum voltage for this battery, should be trended towards to minimize battery degradation.
-VOLTAGE_NORM: float = 3.6
+# https://www.aliexpress.com/item/1005002877506257.html?spm=a2g0o.productlist.0.0.9b4551fdEulDv5&algo_pvid=8c6cf013-c144-455b-ad81-7fe01dc505a4&algo_exp_id=8c6cf013-c144-455b-ad81-7fe01dc505a4-0&pdp_ext_f=%7B%22sku_id%22%3A%2212000022586204386%22%7D&pdp_npi=2%40dis%21EUR%2111.93%219.54%21%21%21%21%21%40210318b816684264364232188e374e%2112000022586204386%21sea&curPageLogUid=fgLi3LoKrm6H
 # At this voltage the battery is dead, trending towards this voltage indicates a lower capacity.
 VOLTAGE_MIN: float = 3.0
 # If voltage trends towards this value, it leads to degradation.
@@ -8,14 +7,14 @@ VOLTAGE_MAX: float = 4.2
 CHARGE_C: float = 1
 # Max rate of discharge.
 DISCHARGE_C: float = 1
+# The threshold of the current when battery is fully charged
+THRESHOLD: float = 0.05
 # The temperature at which the battery will keep increasing in temperature and go on fire.
 THERMAL_RUNAWAY: float = 105
 # The battery's status, either dead or alive. 
 BATTERY_ALIVE: bool = True
 # The original capacity of the battery.
 CAPACITY: float = 4352
-# The threshold of the current when battery is fully charged
-THRESHOLD: float = 0.05
 # Estimate of how many full charge and discharge cycles the battery can handle before it begins to lose functional capacity.
 lifespan: float = 1
 # variable to hold charge percent
