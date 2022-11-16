06a1e474cdef2e9efeaa3b8d4280cfb3b98779db
EimantasPusinskas <67287556+EimantasPusinskas@users.noreply.github.com>
2022-11-16 11:19:03 +0000

    Metrics.py change
    time_since_last_soc_calculation moved to config.py

--- ms_modules/metrics.py
+++ ms_modules/metrics.py
@@ -1,7 +1,7 @@
 from time import time
 import config
 
-time_since_last_soc_calculation: time
+time_since_last_soc_calculation: time = time()
 battery_percentage: float
 functional_capacity: float
 functional_capacity = config.CAPACITY * config.lifespan
@@ -26,7 +26,7 @@ def time_till_empty(phone, battery_percentage, functional_capacity) -> float:
     return tte
 
 
-def state_of_charge(charger, phone, battery_percentage, time_since_last_soc_calculation, functional_capacity) -> float:
+def state_of_charge(charger, phone, battery_percentage, functional_capacity) -> float:
     """
     state_of_charge returns the current battery percentage 
 
@@ -41,9 +41,9 @@ def state_of_charge(charger, phone, battery_percentage, time_since_last_soc_calc
     # gets current time t
     time_now = time()
     #time_step is the time interval between the last SoC calculation at t-1 and the current SoC calculation at time t
-    time_step = time_now - time_since_last_soc_calculation
+    time_step = time_now - config.time_since_last_soc_calculation
     # updates the time since the last soc calculation to the current time
-    time_since_last_soc_calculation = time_now
+    config.time_since_last_soc_calculation = time_now
     # calculates the state of charge
     soc = battery_percentage + ((i/(functional_capacity / 1000)) * time_step)
     # sets the battery percentage equal to the newly calculated battery percentage
