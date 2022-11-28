22b08b3b676fd31442886ccbaa2365e71f86186a
Matt Mallen <120355103@umail.ucc.ie>
2022-11-28 20:04:15 +0000

    Finished docstrings in PowerBrick class.

--- classes/powerbrick.py
+++ classes/powerbrick.py
@@ -2,21 +2,21 @@
 class PowerBrick:
     def __init__(self, CURRENT: float, VOLTAGE: float, ) -> None:
         """
-        Initializes an instance of the Charger class based on the arguments you pass to this constructor.
+        Initializes an instance of the PowerBrick class based on the arguments you pass to this constructor.
 
         Keyword arguments:
-        CURRENT -- constant, the current of the charger.
-        VOLTAGE -- constant, the voltage of the charger.
+        CURRENT -- constant, the current of the PowerBrick.
+        VOLTAGE -- constant, the voltage of the PowerBrick.
         """
         self._CURRENT = CURRENT
         self._VOLTAGE = VOLTAGE
 
     def _get_current(self) -> float:
-        """Returns the current of the charger."""
+        """Returns the current of the PowerBrick."""
         return self._CURRENT
 
     def _get_voltage(self) -> float:
-        """Returns the voltage of the charger"""
+        """Returns the voltage of the PowerBrick."""
         return self._VOLTAGE
     # Assign all of the getters to class properties. No setters as all of the class' attribtutes are constants.
     # This means private instance variables can be accessed "directly" by using the getters and setters as an interface.
